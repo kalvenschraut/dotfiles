857753db20f66b38a88bcb7c5edb621e368354be
Kalven Schraut <kalvens@rtvision.com>
2023-07-19 09:25:13 -0500

    feat(nvim): add rehighlight text when shifting text left/right

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -4,6 +4,8 @@ vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 -- to move hightlighted text in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
