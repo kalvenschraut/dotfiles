08f6e03e7251e2bbd6680e72392005555d04a99e
Kalven Schraut <kalvens@rtvision.com>
2023-08-18 21:10:21 -0500

    feat(nvim): ignore and smart casing enabled

--- nvim/lua/config/set.lua
+++ nvim/lua/config/set.lua
@@ -20,6 +20,9 @@ vim.opt.backupdir = os.getenv('HOME') .. '/.nvim/backups'
 
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
+vim.opt.smartcase = true
+vim.opt.ignorecase = true
+
 
 vim.opt.termguicolors = true
 
