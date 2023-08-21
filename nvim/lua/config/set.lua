e685a36dac26ab4c993ce35c16ee8ded13ffc017
Kalven Schraut <kalvens@rtvision.com>
2023-08-21 08:30:45 -0500

    feat(nvim): files reopen where they last were opened at

--- nvim/lua/config/set.lua
+++ nvim/lua/config/set.lua
@@ -57,3 +57,12 @@ augroup templates
     autocmd BufNewFile *.vue 0r ~/.nvim/templates/sfc.vue
 augroup END
 ]])
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+	pattern = { "*" },
+	callback = function()
+		if vim.fn.line("'\"") > 1 and vim.fn.line("'\"") <= vim.fn.line("$") then
+			vim.api.nvim_exec("normal! g'\"", false)
+		end
+	end
+})
