50cfdbd2cc74c0531f4ee7cb341761a4a67b6069
Kalven Schraut <kalvens@rtvision.com>
2024-03-01 17:00:21 -0600

    smart indent settings:

--- nvim/lua/config/set.lua
+++ nvim/lua/config/set.lua
@@ -11,7 +11,8 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = false
 vim.opt.smartindent = true
 vim.opt.wrap = false
-
+vim.opt.autoindent = true
+vim.opt.smartindent = true
 
 vim.opt.undofile = true
 vim.opt.undodir = os.getenv('HOME') .. '/.nvim/undo'
