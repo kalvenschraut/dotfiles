69aeabba971ac4facdc040b055c0821de1a922ef
Kalven Schraut <kalvens@rtvision.com>
2024-03-15 11:49:44 -0500

    harpoon keybinding changes

--- nvim/lua/plugin/harpoon.lua
+++ nvim/lua/plugin/harpoon.lua
@@ -5,11 +5,11 @@ return {
 		local ui = require('harpoon.ui');
 
 		vim.keymap.set('n', '<leader>a', mark.add_file);
-		vim.keymap.set('n', '<c-e>', ui.toggle_quick_menu);
+		vim.keymap.set('n', '<leader>e', ui.toggle_quick_menu);
 
-		vim.keymap.set('n', '<c-1>', function() ui.nav_file(1) end);
-		vim.keymap.set('n', '<c-2>', function() ui.nav_file(2) end);
-		vim.keymap.set('n', '<c-3>', function() ui.nav_file(3) end);
-		vim.keymap.set('n', '<c-4>', function() ui.nav_file(4) end);
+		vim.keymap.set('n', '<leader>1', function() ui.nav_file(1) end);
+		vim.keymap.set('n', '<leader>2', function() ui.nav_file(2) end);
+		vim.keymap.set('n', '<leader>3', function() ui.nav_file(3) end);
+		vim.keymap.set('n', '<leader>4', function() ui.nav_file(4) end);
 	end
 }
