46351cb672e1ad70185e39c0b6bcc66337a8133c
Kalven Schraut <kalvens@rtvision.com>
2025-01-17 17:28:19 -0600

    refactor: remove lsp-zero dependency to fix nvim-cmp enter mapping issue

--- nvim/lua/plugin/noice.lua
+++ nvim/lua/plugin/noice.lua
@@ -11,6 +11,7 @@ return {
 					["vim.lsp.util.stylize_markdown"] = true,
 					["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
 				},
+				hover = { silent = true }
 			},
 			-- you can enable a preset for easier configuration
 			presets = {
