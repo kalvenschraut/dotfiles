5a6956bf835d8c93412650ad03c7aed10fb154b2
Kalven Schraut <kalvens@rtvision.com>
2026-04-21 12:03:21 -0500

    feat: update oxfmt/oxlint lsp configs and tmux init script

--- nvim/after/lsp/oxlint.lua
+++ nvim/after/lsp/oxlint.lua
@@ -1,9 +1,4 @@
 return {
-	root_dir = function(bufnr, on_dir)
-		local root = vim.fs.root(bufnr,
-			{ 'oxlint.config.ts', '.oxlintrc.json', '.oxlintrc.jsonc', 'package.json', '.git' })
-		on_dir(root or vim.fn.getcwd())
-	end,
 	cmd = function(dispatchers, config)
 		return vim.lsp.rpc.start({ 'pnpm', 'exec', 'oxlint', '--lsp' }, dispatchers, {
 			cwd = config.root_dir
--- nvim/lsp/oxfmt.lua
+++ nvim/lsp/oxfmt.lua
@@ -1,10 +1,5 @@
-local masonOxfmt = vim.fn.stdpath('data') .. '/mason/bin/oxfmt'
-local cmd = vim.fn.executable(masonOxfmt) == 1
-	and { masonOxfmt, '--lsp' }
-	or { 'oxfmt', '--lsp' }
-
 return {
-	cmd = cmd,
+	cmd = { 'oxfmt', '--lsp' },
 	filetypes = {
 		'javascript',
 		'javascriptreact',
@@ -13,10 +8,7 @@ return {
 		'typescriptreact',
 		'typescript.tsx',
 		'vue',
-		'json'
-	},
-	root_dir = function(bufnr, on_dir)
-		local root = vim.fs.root(bufnr, { '.oxlintrc.json', 'package.json', '.git' })
-		on_dir(root or vim.fn.getcwd())
-	end,
+		'json',
+		'md'
+	}
 }
